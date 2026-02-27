4a416d28dc53c55179df3f16afebd8feffefc972
Harshith A K <harshithak5520@gmail.com>
2026-02-27 20:23:56 +0530

    Implement longestCommonPrefix method in Solution class

---
+++ 014.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        first=strs[0]
+        for i in range(len(first)):
+            char=first[i]
+            for word in strs:
+                if i>=len(word) or word[i]!=char:
+                    return first[:i]
+        return first
+        
